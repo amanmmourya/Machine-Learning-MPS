8d329e1ffafffcd8dcf3125129d6a6a9687205e2
amanmmourya <mouryaaman69@gmail.com>
2025-06-29 22:40:29 +0530

    added uvicorn

--- frontend/app/response/page.js
+++ frontend/app/response/page.js
@@ -27,7 +27,7 @@ const Response = () => {
   
       // Wrap everything in an async function
       (async () => {
-          const NEXT_BACKEND_URL=process.env.NEXT_PUBLIC.BACKEND_URL || 'http://localhost:8000';
+          const NEXT_BACKEND_URL=process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000';
           const possibleDiseaseFunction = async () => {
               const response = await fetch(`${NEXT_BACKEND_URL}/giveDisease`, {
                   method: 'POST',
